1d04c4b99f1a2d793345bebb0e1a479b93574349
Mark Disterhof <markdisterhof@outlook.de>
2022-02-26 11:19:06 +0100

    DMRS EQ

--- nrphypy/decode.py
+++ nrphypy/decode.py
@@ -15,7 +15,7 @@ def _pss_candidates(N):
     return pss_candidates_t
 
 
-def sync_pss(received_data: np.ndarray, fft_size: int,threshold:float, pss_candidates: np.ndarray = None) -> np.ndarray:
+def sync_pss(received_data: np.ndarray, fft_size: int, threshold: float, pss_candidates: np.ndarray = None) -> np.ndarray:
     """TODO Process time domain samples to return array of 2d ssbs 
 
     Args:
@@ -24,32 +24,33 @@ def sync_pss(received_data: np.ndarray, fft_size: int,threshold:float, pss_candi
 
     Returns:
         np.ndarray: [description]
-    """    
+    """
     # 3x 1d vector containing each ssb nid2s, offsets in rec_data and freq offsets
-    nid2, epsilon, sample_offs = freq_time_sync(received_data= received_data,fft_size= fft_size,threshold= None,pss_candidates = pss_candidates)
-    
+    nid2, epsilon, sample_offs = freq_time_sync(
+        received_data=received_data, fft_size=fft_size, threshold=None, pss_candidates=pss_candidates)
     sample_cut = len(received_data)  # no of processed sample
-    if len(sample_offs) == 0:
-        return np.array([],dtype=complex),[], sample_cut
 
-    if sample_offs[len(sample_offs)-1] + 4 * fft_size > len(received_data):
+    while not len(sample_offs) == 0 and  sample_offs[len(sample_offs)-1] + 4 * fft_size > len(received_data):
         # case received_data contains pss at the end but not rest of ssb
         # remove from found ssbs, sample_cut is no of processed samples (to be removed from memory)
         nid2 = nid2[:len(nid2)-1]
         sample_cut = sample_offs[len(sample_offs)-1]
         sample_offs = sample_offs[:len(sample_offs)-1]
-        epsilon = epsilon[:len(epsilon)-1]
+        epsilon = epsilon[:len(epsilon)-1]  
 
-    
+    if len(sample_offs) == 0:
+        return np.array([], dtype=complex), [], sample_cut, []
+
     ssbs_f = np.zeros(shape=(len(sample_offs), 4, fft_size), dtype=complex)
     for i, offs in enumerate(sample_offs):
         ssbs_f[i] = received_data[offs:offs+4*fft_size].reshape(4, fft_size)
     for i_ssb in range(len(ssbs_f)):
         for sym in range(4):
-            ssbs_f[i_ssb,sym] *= np.exp(-1*epsilon[i_ssb]*1j*2*np.pi*np.arange(fft_size)/fft_size)
-            ssbs_f[i_ssb,sym] = np.fft.fft(ssbs_f[i_ssb,sym])
-    gr = np.array([ssbs_f[i].T for i in range(len(ssbs_f))])[:,:240,:]
-    return gr, nid2, sample_cut
+            ssbs_f[i_ssb, sym] *= np.exp(-1*epsilon[i_ssb]
+                                         * 1j*2*np.pi*np.arange(fft_size)/fft_size)
+            ssbs_f[i_ssb, sym] = np.fft.fft(ssbs_f[i_ssb, sym])
+    gr = np.array([ssbs_f[i].T for i in range(len(ssbs_f))])[:, :240, :]
+    return gr, nid2, sample_cut, sample_offs
 
 
 def freq_time_sync(received_data: np.ndarray, fft_size: int, threshold: float, pss_candidates: np.ndarray = None) -> [int, int, int]:
@@ -67,6 +68,9 @@ def freq_time_sync(received_data: np.ndarray, fft_size: int, threshold: float, p
 
     # prepare
     rec = np.array(received_data).flatten()
+    if np.var(rec) == 0.0:
+        return [],[],[]
+    rec /= np.average(rec)
     # if pss_candidate provided do not compute iffts again etc.
     if pss_candidates == None:
         pss_candidates = _pss_candidates(fft_size)
@@ -93,38 +97,52 @@ def freq_time_sync(received_data: np.ndarray, fft_size: int, threshold: float, p
         if np.average(B) <= 0.00001:
             # avoid avg(B)== for for next if case (div by zero)
             threshold = 1.
-        elif np.max(B)/ np.average(B)/1.1 < 10.:
-             # case only noise, no meaningful threshold, threshold higher than 
+        elif np.max(B) / np.average(B)/1.1 < 10.:
+            # case only noise, no meaningful threshold, threshold higher than
             threshold = np.max(B) + 1.
         else:
-            threshold = np.max(B)- np.average(B)*1.1
-    
+            threshold = np.max(B) - np.average(B)*1.1
+
     peaks = np.argwhere(B >= threshold)
-    
+
     if len(peaks) > 100:
-        raise Warning('threshold unreasonably low, too much memory is going to be allocated. threshold: {}, max(B): {}\ncontinuing with:{}'.format(threshold, np.max(B), np.max(B)*0.9))
-        return freq_time_sync(rec,fft_size,np.max(B)*0.9,pss_candidates)
+        raise Warning('threshold unreasonably low, too much memory is going to be allocated. threshold: {}, max(B): {}\ncontinuing with:{}'.format(
+            threshold, np.max(B), np.max(B)*0.9))
+        return freq_time_sync(rec, fft_size, np.max(B)*0.9, pss_candidates)
 
     ffo = np.angle(C[peaks[:, 0], peaks[:, 2]])/np.pi/2
     epsilon = peaks[:, 2] + ffo
     return [peaks[:, 0], epsilon, peaks[:, 1]]
 
 
-def pss_correlate(ofdm_sym: Union[np.ndarray, list]) -> [int, int, int]:
-    """Correlate PSS on given data
+def pss_correlate(received_data: np.ndarray, fft_size: int, pss_candidates: np.ndarray = None) -> int:
+    
+    # prepare
+    rec = np.array(received_data).flatten()
+    if np.var(rec) == 0.0:
+        return [],[],[]
+    rec /= np.average(rec)
+    # if pss_candidate provided do not compute iffts again etc.
+    if pss_candidates == None:
+        pss_candidates = _pss_candidates(fft_size)
+    #
+    '''
+    B[t,n,k], where:
+    t in {0,1,2}, NID2
+    n in {0, len(rec)- len(pss_sequence)} cross-correlation sequence, which calculated starting from the n-th point of the receiving sequence
+    k in {0,..,fft_size} fft point index
+    '''
 
-    Args:
-        ofdm_sym (Union[np.ndarray, list]): One OFDM Symbol
+    mod_t = np.array([np.exp(k*1j*2*np.pi*np.arange(fft_size)/fft_size)
+                     for k in range(fft_size)])  # modulator for integer freq shift
+    B = np.zeros((len(pss_candidates), len(rec) -
+                 fft_size + 1, fft_size), dtype=float)
 
-    Returns:
-        [int, int, int]: NID_2, k_ssb, max_correlation
-    """
-
-    ofdm_sym = ofdm_sym.flatten()
-    corr = np.array([np.correlate(np.real(ofdm_sym), signals.pss(nid2))
-                    for nid2 in range(3)], dtype=float)
-    (nid2, pss_start) = np.unravel_index(np.argmax(corr), corr.shape)
-    return nid2, pss_start-56, corr[nid2, pss_start]/127.
+    for t in range(len(pss_candidates)):  # for each NID2
+        for k in range(len(B[0, 0])):  # for each carrier offset
+            B[t, :, k] = np.abs(np.correlate(
+                rec, pss_candidates[t] * mod_t[k]))
+    return np.unravel_index(np.argmax(B, axis=None), B.shape)[0]
 
 
 def decode_sss(sss_data: Union[np.ndarray, list], nid2: int) -> int:
@@ -145,8 +163,8 @@ def decode_sss(sss_data: Union[np.ndarray, list], nid2: int) -> int:
     corr = np.zeros(len(sss_candidates), dtype=complex)
 
     for i, sss_i in enumerate(sss_candidates):
-        corr[i] = np.abs(np.multiply(sss_i, sss_data).sum())
-
+        corr[i] = np.abs(np.correlate(sss_data,sss_i))
+        
     return int(np.argmax(corr, axis=None))
 
 
@@ -181,3 +199,24 @@ def decode_pbch(pbch_data: Union[np.ndarray, list], L_max: int, N_ID_Cell: int, 
     scr = [c[i + v * M_bit] for i in range(M_bit)]
 
     return np.array([int(x) for x in np.logical_xor(b, scr)])
+
+def dmrs(dmrs_data: Union[np.ndarray, list], N_ID_Cell:int, L_max:int) -> int:
+    corr = np.array([np.correlate(dmrs_data, can_dmrs) for can_dmrs in [signals.dmrs(x,N_ID_Cell,L_max) for x in range(L_max)]])
+    return np.argmax(np.abs(corr))
+
+
+def dmrs_eq(ssb_data, i_ssb, N_ID_Cell, L_max, deg= 10):
+    ssb_ = np.array(ssb_data)
+    _, dmrs_data = ssb.unmap_pbch(ssb_,N_ID_Cell%4)
+    can = signals.dmrs(i_ssb, N_ID_Cell, L_max)
+    hest0 = dmrs_data/can
+    dmrs_mask = ssb.map_pbch(np.zeros(432), hest0, N_ID_Cell%4)
+    for sym in range(1,4):
+        idx = np.where(dmrs_mask.T[sym] != 0)[0]
+        x = idx
+        y = hest0[:len(idx)]
+        hest0 = hest0[len(idx):]
+        p = np.poly1d(np.polyfit(x,y,deg))
+        ssb_[:,sym] /=p(range(240))
+        
+    return ssb_
